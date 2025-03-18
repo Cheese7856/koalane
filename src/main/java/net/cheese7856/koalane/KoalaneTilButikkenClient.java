cb0e2a6e7a1c42332d751894e87dcc64f725df9f
Elander Olsen <elander.olsen@gmail.com>
2025-03-18 20:45:52 +0100

    Added /koalane command

--- src/main/java/net/cheese7856/koalane/KoalaneTilButikkenClient.java
+++ src/main/java/net/cheese7856/koalane/KoalaneTilButikkenClient.java
@@ -4,6 +4,8 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvType;
 import net.minecraft.client.MinecraftClient;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
 
 @Environment(EnvType.CLIENT)
@@ -13,6 +15,98 @@ public class KoalaneTilButikkenClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		// Registrer keybinding
 		KeybindHandler.register();
+
+		// Registrer /ko som en egen kommando (for autokorrekt)
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(
+					ClientCommandManager.literal("ko")
+							.executes(context -> {
+								MinecraftClient client = MinecraftClient.getInstance();
+								if (client.player != null) {
+									client.player.networkHandler.sendCommand("warp Butikken");
+								}
+								return 1;
+							})
+			);
+		});
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(
+					ClientCommandManager.literal("koa")
+							.executes(context -> {
+								MinecraftClient client = MinecraftClient.getInstance();
+								if (client.player != null) {
+									client.player.networkHandler.sendCommand("warp Butikken");
+								}
+								return 1;
+							})
+			);
+		});
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(
+					ClientCommandManager.literal("koal")
+							.executes(context -> {
+								MinecraftClient client = MinecraftClient.getInstance();
+								if (client.player != null) {
+									client.player.networkHandler.sendCommand("warp Butikken");
+								}
+								return 1;
+							})
+			);
+		});
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(
+					ClientCommandManager.literal("koal")
+							.executes(context -> {
+								MinecraftClient client = MinecraftClient.getInstance();
+								if (client.player != null) {
+									client.player.networkHandler.sendCommand("warp Butikken");
+								}
+								return 1;
+							})
+			);
+		});
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(
+					ClientCommandManager.literal("koala")
+							.executes(context -> {
+								MinecraftClient client = MinecraftClient.getInstance();
+								if (client.player != null) {
+									client.player.networkHandler.sendCommand("warp Butikken");
+								}
+								return 1;
+							})
+			);
+		});
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(
+					ClientCommandManager.literal("koalan")
+							.executes(context -> {
+								MinecraftClient client = MinecraftClient.getInstance();
+								if (client.player != null) {
+									client.player.networkHandler.sendCommand("warp Butikken");
+								}
+								return 1;
+							})
+			);
+		});
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(
+					ClientCommandManager.literal("koalane")
+							.executes(context -> {
+								MinecraftClient client = MinecraftClient.getInstance();
+								if (client.player != null) {
+									client.player.networkHandler.sendCommand("warp Butikken");
+								}
+								return 1;
+							})
+			);
+		});
 
 		// Erstatt alle varianter av /warp ko, koa, koalane
 		ClientSendMessageEvents.ALLOW_COMMAND.register(command -> {
